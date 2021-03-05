ac396d8430af5bc0aa27560238609f570ca31cd9
DannyCastroVentura <dannycastroventura@gmail.com>
2021-03-05 18:38:58 +0000

    lib/commands

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ client.on("message", (message) => {
     if(!comecaComPrefixo) return;
 
     
-    const {commands} = require("./lib/commands.js")(message, args, command);
+    const commands = require("./lib/commands")(message, args, command);
 
     Object.keys(commands).includes(command)?commands[command]():message.reply("não entendi meu filho. Faz «ajudai» para mais informações.");
 });
