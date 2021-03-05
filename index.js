c62272e22033f2a6f79c2fda947d4dcc65f8c394
DannyCastroVentura <dannycastroventura@gmail.com>
2021-03-05 17:15:01 +0000

    lib/arrays.js

--- index.js
+++ index.js
@@ -14,12 +14,8 @@ const client = new Discord.Client();
 
 const prefix = "DEUS ";
 
-const asneiras = ['merda', 'chupaimos', 'puta', 'cabra', 'vaca', 'fodasse', 'foder', 'fuder', 'cabrão', 'crl', 'mrd'];
-const pilas = ['pixa', 'pila', 'caralho', 'picha'];
-const conas = ['cona', 'kona', 'pussy', 'paxaxa', 'paxaxinha', 'grelo'];
-const ofensa = ['gay', 'homosexual', 'paneleiro', 'lesbica'];
-const perdao = ['desculpa', 'perdão'];
-const respostasAoDunkMemer = ['pls boobs', 'pls ass', 'pls porn', 'pls tits', 'pls booty'];
+include("./lib/arrays.js");
+
 
 client.on("message", (message) => {
 
